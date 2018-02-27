b25fb7358005b361e87b12d61016cd25bd2ca2e0
Tadaboody <tomerpet@gmail.com>
2018-02-27 20:46:16 +0200

    Store options visibility
    - Store it locally instead of on inside the html attributes
    - explicitly set it to boolean instead of an int (this isn't C)

--- filter.js
+++ filter.js
@@ -7,32 +7,38 @@ function htmlToElement(html) {
     return template.content.firstChild;
 }
 
-function add_controls(show){
+function toggle_options(img) {
+	let open_img = 'https://image.flaticon.com/icons/svg/118/118738.svg';
+	let close_img = 'https://image.flaticon.com/icons/svg/126/126492.svg';
+	if (img.toggled) {
+		cbxs.style.display = 'none';
+		img.setAttribute('optionsVisible', false);
+		img.src = close_img;
+	} else {
+		cbxs.style.display = '';
+		img.src = open_img;
+	}
+	img.toggled = !img.toggled;
+	chrome.storage.sync.set({
+		'optionsVisible': img.toggled
+	});
+}
+
+function add_controls(show, optionsVisible) {
 	var control_element = htmlToElement('<fieldset> <legend>בחר איזה קורסים להציג</legend> <div id="cbxs"> <div> <input type="checkbox" id="in-a" name="a" value="סמסטר א"> <label for="a">סמסטר א</label> </div> <div> <input type="checkbox" id="in-b" name="b" value="סמסטר ב" checked> <label for="b">סמסטר ב</label> </div> <div> <input type="checkbox" id="in-c" name="c" value="סמסטר קיץ"> <label for="c">סמסטר קיץ</label> </div> <div> <input type="checkbox" id="in-x" name="x" value="אחר"> <label for="x">כל דבר שהוא לא קורס - ספריה וכו</label> </div> </div> </fieldset>');
 
 	var course_box = document.getElementsByClassName('courses frontpage-course-list-enrolled')[0];
-	var frontpage = document.getElementById('frontpage-course-list');
-	frontpage.insertBefore(control_element, course_box);
+	var front_page = document.getElementById('frontpage-course-list');
+	front_page.insertBefore(control_element, course_box);
 
 	var img = document.createElement('img');
-	var open_img = 'https://image.flaticon.com/icons/svg/118/118738.svg';
-	var close_img = 'https://image.flaticon.com/icons/svg/126/126492.svg';
-	img.src = open_img;
 	img.width = img.height = 16;
-	img.setAttribute('shown', 1);
+	img.toggled = !optionsVisible; //start in the opposite state
+	toggle_options(img); // initialize to the real state by toggling
 	control_element.insertBefore(img, control_element.firstChild);
 	var cbxs = control_element.querySelector('#cbxs');
-	img.onclick = function(){
-		if(img.getAttribute('shown') == 1){
-			cbxs.style.display = 'none';
-			img.setAttribute('shown', 0);
-			img.src = close_img;
-		}
-		else{
-			cbxs.style.display = '';
-			img.setAttribute('shown', 1);
-			img.src = open_img;
-		}
+	img.onclick = function () {
+		toggle_options(img);
 	};
 
 
@@ -43,11 +49,11 @@ function add_controls(show){
 
 	chck_boxes = [in_a, in_b, in_c, in_x];
 
-	for(let cb of chck_boxes){
+	for (let cb of chck_boxes) {
 		cb.checked = show[cb.name];
 	}
 
-	function changed(cb){
+	function changed(cb) {
 		show[cb.name] = cb.checked;
 		chrome.storage.sync.set({
 			"show": show
@@ -56,7 +62,7 @@ function add_controls(show){
 		});
 	}
 
-	for(let cb of chck_boxes){
+	for (let cb of chck_boxes) {
 		cb.addEventListener('change', event => {
 			changed(event.target);
 			course_filter(show);
@@ -65,84 +71,86 @@ function add_controls(show){
 
 	course_filter(show);
 
-}
-
-
-var courses;
-var semester = [];
-
-function classify_courses(){
-	var all_regex = /^[א-ת\s!"#$%&'()*+,./:;<=>?@\^_`{|}~-]+[\d\s]+[אבק]\d*$/m;
-
-	var names = [];
-	courses = document.getElementsByClassName('coursebox clearfix');
-
-	for (var i = 0; i < courses.length; i++){
-		var c = courses[i];
-		var name_node = c.getElementsByClassName('coursename')[0];
-		if (name_node == undefined){
-			console.log('name not found');
-			continue;
-		}
-		var name = name_node.innerText;
-
-		names.push(name);
-		if(!all_regex.test(name)){
-			semester[i] = 'x';
-			continue;
-		}
-
-		var last = name.split(' ').pop();
-		if(/[אבק]\d{2}\s/.test(last)){
-			semester[i] = {א:'a', ב:'b', ק:'c'}[last[0]];
-		}
-		else{
-			semester[i] = 'x';
-		}
-
-		delete name;
 	}
-}
 
-function course_filter(show){
 
-	var shown = 0;
-	for (var i = 0; i < courses.length; i++){
-		var c = courses[i];
-		if(!show[semester[i]]){
-			c.style.display = 'none';
+	var courses;
+	var semester = [];
+
+	function classify_courses() {
+		var all_regex = /^[א-ת\s!"#$%&'()*+,./:;<=>?@\^_`{|}~-]+[\d\s]+[אבק]\d*$/m;
+
+		var names = [];
+		courses = document.getElementsByClassName('coursebox clearfix');
+
+		for (var i = 0; i < courses.length; i++) {
+			var c = courses[i];
+			var name_node = c.getElementsByClassName('coursename')[0];
+			if (name_node == undefined) {
+				console.log('name not found');
+				continue;
+			}
+			var name = name_node.innerText;
+
+			names.push(name);
+			if (!all_regex.test(name)) {
+				semester[i] = 'x';
+				continue;
+			}
+
+			var last = name.split(' ').pop();
+			if (/[אבק]\d{2}\s/.test(last)) {
+				semester[i] = {
+					א: 'a',
+					ב: 'b',
+					ק: 'c'
+				}[last[0]];
+			} else {
+				semester[i] = 'x';
+			}
+
+			delete name;
 		}
-		else{
-			c.style.display = '';
-			if (shown % 2 == 0){
-				c.classList.remove('even');
-				c.classList.add('odd');
-				
-				if (shown == 0){
-					c.classList.add('first');
+	}
+
+	function course_filter(show) {
+
+		var shown = 0;
+		for (var i = 0; i < courses.length; i++) {
+			var c = courses[i];
+			if (!show[semester[i]]) {
+				c.style.display = 'none';
+			} else {
+				c.style.display = '';
+				if (shown % 2 == 0) {
+					c.classList.remove('even');
+					c.classList.add('odd');
+
+					if (shown == 0) {
+						c.classList.add('first');
+					}
+				} else {
+					c.classList.remove('odd');
+					c.classList.add('even');
 				}
+				shown++;
 			}
-			else{
-				c.classList.remove('odd');
-				c.classList.add('even');		
-			}
-			shown++;
 		}
 	}
-}
 
-function init() {
-	var default_show = {
-		a: false,
-		b: true,
-		c: false,
-		x: false
-	};
-	chrome.storage.sync.get("show", results => {
-		let show = results.show || default_show;//first value that evaluates to true (not undefined)
-		classify_courses();
-		add_controls(show);
-	});
-}
-
+	function init() {
+		var default_show = {
+			a: false,
+			b: true,
+			c: false,
+			x: false
+		};
+		chrome.storage.sync.get(null, storage => { // null - get entire storage
+			let show = storage.show || default_show; //first value that evaluates to true (not undefined)
+			let optionsVisible = storage.optionsVisible;//x || true will always give true
+			if(optionsVisible == undefined) optionsVisible = true;//default to true
+			classify_courses();
+			add_controls(show, optionsVisible);
+		});
+	}
 window.addEventListener('load', init);
